23ce92bf51adf2866df40dbbf550d77beab65afb
Josh Stow <55459228+joshstow@users.noreply.github.com>
2020-12-30 15:32:21 +0000

    Removed log file dependency for Heroku compatibility

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 BOT_USER = 'StockrAI'
 WAIT_TIME = 15
+LAST_ID = 1344268090406809603
 #SECRETS_FILE = 'secrets.json'
-LOG_FILE = 'tweet_log.csv'
+#LOG_FILE = 'tweet_log.csv'
 
 # def load_secrets():
 #     """
@@ -56,24 +57,25 @@ def send_tweets(api, mentions):
             print(f"""++ New mention @ {mention['timestamp']} {{'id': {mention['id']}, 'username': '{mention['username']}', 'text': '{mention['text']}'}}""")
             body = construct_tweet(mention['username'], ticker) # Construct body of tweet
             api.update_status(body, mention['id'])  # Post tweet
-            log(mention)
+            #log(mention)
+    return mention['id']
 
-def get_last_id():
-    """
-    Retrieve last tweet ID from text file
-    """
-    with open(LOG_FILE, 'r') as f: 
-        data = f.readlines()[-1].split(',')
-        return data[1]
+# def get_last_id():
+#     """
+#     Retrieve last tweet ID from text file
+#     """
+#     with open(LOG_FILE, 'r') as f: 
+#         data = f.readlines()[-1].split(',')
+#         return data[1]
 
-def log(mention):
-    """
-    Append mentioned tweet data to log csv file
-    """
-    with open(LOG_FILE, 'a', newline='') as f:
-        writer = csv.writer(f)
-        line = [mention['timestamp'],mention['id'],mention['username'],mention['text']]
-        writer.writerow(line)
+# def log(mention):
+#     """
+#     Append mentioned tweet data to log csv file
+#     """
+#     with open(LOG_FILE, 'a', newline='') as f:
+#         writer = csv.writer(f)
+#         line = [mention['timestamp'],mention['id'],mention['username'],mention['text']]
+#         writer.writerow(line)
 
 def scan_tweet(text):
     """
@@ -104,7 +106,7 @@ Adjusted Close: {data['adj_close']}
     except:
         body = f"""
 @{username}
-Sorry, ${ticker.upper()} is not a supported stock :(
+Sorry, ${ticker.upper()} is not a supported ticker :(
 """
     return body
 
@@ -114,6 +116,6 @@ if __name__ == '__main__':
     print(f'== Now scanning for tweets mentioning @{BOT_USER}')
     # Constantly scan for new tweets mentioning bot account
     while True:
-        mentions = [{'timestamp':mention.created_at,'id':mention.id,'username':mention.user.screen_name,'text':mention.text} for mention in api.mentions_timeline(since_id=get_last_id())]  # Create list of dictionaries for each mention tweet since last ID
-        send_tweets(api, mentions)  # Post tweet replies
+        mentions = [{'timestamp':mention.created_at,'id':mention.id,'username':mention.user.screen_name,'text':mention.text} for mention in api.mentions_timeline(since_id=LAST_ID)]  # Create list of dictionaries for each mention tweet since last ID
+        LAST_ID = send_tweets(api, mentions)  # Post tweet replies and get last ID
         time.sleep(WAIT_TIME)
